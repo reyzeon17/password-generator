2a9bf8c7777e7e71ab030041c7d859edf08f5bce
reyzeon17 <reyzeonrio@gmail.com>
2023-11-07 21:28:08 +0800

    uploading files

---
+++ index.js
@@ -0,0 +1,113 @@
+const characters = [
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"~",
+	"`",
+	"!",
+	"@",
+	"#",
+	"$",
+	"%",
+	"^",
+	"&",
+	"*",
+	"(",
+	")",
+	"_",
+	"-",
+	"+",
+	"=",
+	"{",
+	"[",
+	"}",
+	"]",
+	",",
+	"|",
+	":",
+	";",
+	"<",
+	">",
+	".",
+	"?",
+	"/",
+];
+
+let btnEl = document.getElementById("btn-el");
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+
+function generate() {
+	passwordOne.textContent = " ";
+	passwordTwo.textContent = " ";
+	for (i = 0; i < 15; i++) {
+		passwordOne.textContent += characters[randomCharacter()];
+	}
+	for (i = 0; i < 15; i++) {
+		passwordTwo.textContent += characters[randomCharacter()];
+	}
+}
+
+function randomCharacter() {
+	let variable = Math.floor(Math.random() * characters.length);
+	return variable;
+}
